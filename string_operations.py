5a7b1ee6d4abfb03bfc047982e9dc9c52969ed2a
paola <swan.madariaga@gmail.com>
2022-09-02 11:06:47 -0400

    Se creo archivo phyton

---
+++ string_operations.py
@@ -0,0 +1,11 @@
+# String concatenation (Juntar Strings)
+
+word_one = "Hello"
+
+space = " "
+
+word two = "World"
+
+sentence = word one + space + word two
+
+print(sentence)
---
+++ variables.py
@@ -0,0 +1,22 @@
+# Una variable es un nombre que apunta a un valor
+# los valores son de cierto tipo
+
+message = "Esto es una mensaje"
+
+age = 76
+
+# Los nombre de las variables no deben contener espacios
+# Los puede separar con "_" o underscore
+dolar_to_peso = 890.2
+
+print(message)
+print(age)
+print(dolar_to_peso)
+
+# Los nombres de las variables NO pueden partir con numeros
+
+# 76Trumpets = "Gran carnaval" = Esto es un nombre invalido
+
+# Tampoco puede utilizar las palabras reservadas del lenguaje
+
+# if = "¿si?" = Esto tampoco es un nombre valido
