cfab978477762ac28a1fff78bf0acaf944ccca4d
paola <swan.madariaga@gmail.com>
2022-09-21 17:48:20 -0400

    refactorisar

--- file_handling/all_together.py
+++ file_handling/all_together.py
@@ -1,38 +1,33 @@
-print("Bievenido al programa")
+print("Bienvenido al programa")
 user_input = ""
-
 def write_file():
-    file = open("file_handling/demo_two.txt","a")
+    file = open("file_handling/demo_two.txt", "a")
     user_content = input("Ingresa el contenido\n")
-    file.write(user_content + "\n")
+    file.write(user_content +"\n")
     file.close()
-
 def read_file():
-    file = open("file_handling/demo_two.txt","r")    
+    file = open("file_handling/demo_two.txt", "r")
     for line in file:
-        print(line)
-
+        print (line)
 def print_menu():
-    print("##################")
-    print("ingresa una opciòn")
-    print("1", "agrega contenido")
+    print("#################")
+    print("ingresa una opción")
+    print("1", "agregar contenido")
     print("2", "leer contenido")
     print("exit", "para salir")
-    print("###################")
-
+    print("################")
 while user_input != "exit":
     print_menu()
     user_input = input()
-
     if user_input == "1":
-        write_file()
+       write_file()
     elif user_input == "2":
-        read_file()
+       read_file()
     elif user_input == "exit":
-        print("Chau chau")
-        exit()
+      print("Chau chau")
+      exit()
     else:
-        print("opciòn incorrecta")
+         print ("opción incorrecta")
         
 
   
